db35571bb20ee9d5415768b3fec6359bcadf24a7
Haubir <jagheterhaubir@gmail.com>
2017-04-24 23:18:38 +0200

    Second commit

---
+++ FirstApp/FirstApp/Main.cs
@@ -0,0 +1,26 @@
+﻿// /*
+//  * Project: /Users/Haubir/XamarinProjects/FirstApp/FirstApp 
+//  * Class:   Main
+//  *
+//  * Version info
+//  * Created: 4/24/2017
+//  * Creator: Haubir - haubir95@hotmail.com
+//  *
+//  * Copyright notice
+//  * Property of Haubir Mariwani. Do not copy, alter, distribute, or sell in any way unless given explicit permission.
+//  */
+using UIKit;
+
+namespace FirstApp
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
---
+++ FirstApp/FirstApp/ViewController.cs
@@ -0,0 +1,37 @@
+﻿// /*
+//  * Project: /Users/Haubir/XamarinProjects/FirstApp/FirstApp 
+//  * Class:   ViewController
+//  *
+//  * Version info
+//  * Created: 4/24/2017
+//  * Creator: Haubir - haubir95@hotmail.com
+//  *
+//  * Copyright notice
+//  * Property of Haubir Mariwani. Do not copy, alter, distribute, or sell in any way unless given explicit permission.
+//  */
+using System;
+
+using UIKit;
+
+namespace FirstApp
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
